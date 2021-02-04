8485f678790b34dcd83e1a92e8dfb1021cf2a275
Grace Flitsch <graceflitsch@graces-air-2.lan>
2021-02-03 20:27:30 -0800

    Added time tests and recorded outcomes

--- experiments.c
+++ experiments.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include "array_util.h"
+#include "sorts.h"
 
 int main(void)
 {
-	printf("hello experiments\n");
-	return 0;
+  printf("Test Number\tTime Taken\tComparisons\t\n");
+  long fore, aft;
+  for(int i = 1; i<11; i++){
+    int test[5000];
+    random_array(test, 5000);
+    fore = get_time_millis();
+    int a = bubbleSort(test, 5000);
+    aft = get_time_millis();
+    long total = aft - fore;
+    printf("%d\t\t%ld\t\t%d\t\n", i, total, a);
+  }
 }
